23e50256c61e3a00a4667b817cfe045c9def0106
Krzysztof Kaminski <krzysztof.teonte@gmail.com>
2019-05-21 13:34:32 +0100

    snapshot

--- __tests__/IndexPage.spec.js
+++ __tests__/IndexPage.spec.js
@@ -4,12 +4,36 @@ import IndexPage from '../pages/index';
 
 describe('Index Page', () => {
   let wrapper;
+  let appBar;
+  let toolbar;
+  let button;
+  let link;
 
   beforeAll(() => {
     wrapper = mount(<IndexPage/>);
+    appBar = wrapper.find('AppBar');
+    toolbar = wrapper.find('Toolbar');
+    button = wrapper.find('Button');
+    link = wrapper.find('Link');
   });
 
   it('should render as expected', () => {
     expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should render AppBar component', () => {
+    expect(appBar.length).toEqual(1);
+  });
+
+  it('should render Toolbar component', () => {
+    expect(toolbar.length).toEqual(1);
+  });
+
+  it('should render 2 Button components', () => {
+    expect(button.length).toEqual(2);
+  });
+
+  it('should render Link component', () => {
+    expect(link.length).toEqual(1);
   });
 });
